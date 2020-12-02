21ae7befe6179704d3b1632a4a7bff6d0327686f
RobKnowles248 <robert@knowles-family.me.uk>
2020-12-02 19:07:20 +0000

    Changed functionality of playNote function so it does not repeat itself and uses the clearTimeout method to save on memory

--- assets/js/piano.js
+++ assets/js/piano.js
@@ -7,33 +7,36 @@ $(document).ready(function() {
     // Make a constant array with the names of the notes
     const pianoNotes = ["c", "cs", "d", "ds", "e", "f", "fs", "g", "gs", "a", "as", "b"];
 
+    // Function that checks if a note is white or black and gives a class accordingly
+    function whiteOrBlackNote(note) {
+
+        if (note.length == 1) {
+            return "white";
+        } else if (note.length == 2) {
+            return "black";
+        } else {
+            alert("Error: neither black nor white note");
+            return;
+        }
+    }
+
     // Function that will play a note when given the name of that note
     function playNote(note) {
 
         //Check if the input is a string
         if (typeof(note) != "string") {
             console.log("The function playNote didn't receive a string");
-        }
-        
-        //Highlight a white note for 0.5s
-        if (note.length == 1) {
-            $(`#${note}`).addClass("active-white");
-            setTimeout(function() {
-                $(`#${note}`).removeClass("active-white");
-            }, 500);
-        
-        //Highlight a black note for 0.5s
-        } else if (note.length == 2) {
-            $(`#${note}`).addClass("active-black");
-            setTimeout(function() {
-                $(`#${note}`).removeClass("active-black");
-            }, 500);
-
-        //Log that the note string was the wrong length    
-        } else {
-            console.log("The string inputted to playNote was the wrong length");
+            return;
         }
 
+        //Highlight note for 0.5s
+        let colour = whiteOrBlackNote(note);
+        $(`#${note}`).addClass(`active-${colour}`);
+        let timeOut = setTimeout(function() {
+                $(`#${note}`).removeClass(`active-${colour}`);
+                clearTimeout(timeOut);
+            }, 500);
+        
         //Play the sound of that note
         $(`audio#${note}-audio`)[0].play();
     }
